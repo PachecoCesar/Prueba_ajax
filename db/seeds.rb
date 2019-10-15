07825afd420452fd752883202b97d74d7b97525a
PachecoCesar <c86pacheco@gmail.com>
2019-10-15 14:13:42 -0300

    added faker and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Complaint.destroy_all
+
+Company.destroy_all
+
+User.destroy_all
+
+20.times do |i|
+   Company.create(
+       name: Faker::Company.name
+   )
+end
+30.times do |j|
+   User.create(
+   email: Faker::Internet.email,
+   password: '123456'
+   )
+end
+250.times do |k|
+   Complaint.create(
+   detail: Faker::Quote.yoda,
+   company: Company.order("RANDOM()").first,
+   user: User.order("RANDOM()").first
+   )
+end
+
+#AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
